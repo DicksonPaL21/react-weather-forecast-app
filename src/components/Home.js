1bbdc025d6a3ac017c76b1bcab0e7bc492e8030b
Dickson Palomeras <dicksonpal_021@yahoo.com.ph>
2022-04-03 10:52:44 +0800

    Fix import helpers folder - key sensitive

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import Loader from './Loader';
 
 // Helpers
-import { actions } from './helpers';
+import { actions } from './Helpers';
 
 const Home = ({ ...props }) => {
   const history = useHistory();
