e034b721c8ee4752667cf107cfda304e085dca7d
Dickson Palomeras <dicksonpal_021@yahoo.com.ph>
2022-04-24 16:06:02 +0800

    fix: Use env to auth0 provider props

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 const providerProps = {
-  domain: 'dev-qpeuo7jr.us.auth0.com',
-  clientId: 'HA5tZRjaMXWckVmTUYAA0IoNfP2mBasT',
+  domain: process.env.REACT_APP_AUTH0_DOMAIN,
+  clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
   redirectUri: window.location.origin,
 };
 
